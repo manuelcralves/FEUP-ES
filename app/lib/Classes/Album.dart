29c77a2ce1f400800d14b6facac6c0b7eb8e53c9
bernardoup31 <up202108842@up.pt>
2023-05-01 02:56:16 +0100

    Album and Music classes edited

--- app/lib/Classes/Album.dart
+++ app/lib/Classes/Album.dart
@@ -1,10 +1,38 @@
 import 'Artist.dart';
 import 'Music.dart';
 
-class Album{
+class Album {
   late int _idAlbum;
   late Artist _artist;
-  late List<Music> musics;
+  late List<Music> _musics;
 
+  int getIdAlbum() {
+    return _idAlbum;
+  }
 
+  void setIdAlbum(int idAlbum) {
+    _idAlbum = idAlbum;
+  }
+
+  Artist getArtist() {
+    return _artist;
+  }
+
+  void setArtist(Artist artist) {
+    _artist = artist;
+  }
+
+  List<Music> getMusics() {
+    return _musics;
+  }
+
+  void setMusics(List<Music> musics) {
+    _musics = musics;
+  }
+
+  Album(int idAlbum, Artist artist, List<Music> musics) {
+    _idAlbum = idAlbum;
+    _artist = artist;
+    _musics = musics;
+  }
 }
