f74e6e2d13359149d2633d17f4018e6b12a41479
manuelcralves <62514015+manuelcralves@users.noreply.github.com>
2023-05-20 17:42:11 +0100

    add playlist

--- app/lib/Index/index.dart
+++ app/lib/Index/index.dart
@@ -1,13 +1,11 @@
 import 'package:app/HomePage/HomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:app/profile/profile.dart';
-import 'package:flutter/material.dart';
-import 'package:app/profile/profile.dart';
 import 'package:app/settings/settings.dart';
-import '../login/login_backend.dart';
+import 'package:app/Playlist/Playlist_create_frontend.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-import '../settings/settings.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:app/Playlist/Playlist_Backend.dart';
 
 class Index extends StatefulWidget {
   @override
@@ -26,6 +24,16 @@ class _IndexState extends State<Index> {
     } on FirebaseAuthException catch (e) {
       print(e.code);
     }
+  }
+
+  Stream<dynamic>? playlistsStream;
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch the playlists from Firebase Realtime Database
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+    playlistsStream = Playlist_Backend.getPlaylistsFromFirebase(uid);
   }
 
   @override
@@ -56,10 +64,42 @@ class _IndexState extends State<Index> {
             icon: Icon(Icons.exit_to_app),
             onPressed: signout, // Call the signout function
           ),
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Playlist_Create()),
+              );
+            },
+          ),
         ],
       ),
-      body: Center(
-        child: Text('Welcome to the Index page!'),
+      body: StreamBuilder(
+        stream: playlistsStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // Process and display the playlists data
+            Map<dynamic, dynamic> playlistsData = snapshot.data!.snapshot.value;
+            List<Widget> playlistWidgets = [];
+            if (playlistsData != null) {
+              playlistsData.forEach((key, value) {
+                String playlistTitle = key;
+                // Create a widget for each playlist
+                playlistWidgets.add(ListTile(
+                  title: Text(playlistTitle),
+                ));
+              });
+            }
+            return ListView(
+              children: playlistWidgets,
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
