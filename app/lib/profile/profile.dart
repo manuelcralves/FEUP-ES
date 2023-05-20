90f8b70a67145d0988801d71ef0c85ed7c2eb15c
manuelcralves <62514015+manuelcralves@users.noreply.github.com>
2023-05-20 16:57:56 +0100

    Update profile.dart

--- app/lib/profile/profile.dart
+++ app/lib/profile/profile.dart
@@ -1,6 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  String? email;
+
+  @override
+  void initState() {
+    super.initState();
+    retrieveEmail();
+  }
+
+  Future<void> retrieveEmail() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        email = user.email;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +31,20 @@ class Profile extends StatelessWidget {
         title: Text('Profile'),
       ),
       body: Center(
-        child: Text('Welcome to the Profile page!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Profile page!',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Email: ${email ?? 'Unknown'}',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
