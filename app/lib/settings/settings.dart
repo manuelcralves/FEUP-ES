f6476627d3a1974699d0883651d00cbc6113d828
manuelcralves <62514015+manuelcralves@users.noreply.github.com>
2023-05-20 18:14:47 +0100

    settings options (no funcionalities)

--- app/lib/settings/settings.dart
+++ app/lib/settings/settings.dart
@@ -7,8 +7,86 @@ class Settings extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Text('Welcome to the Settings page!'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'General Settings',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+              SwitchListTile(
+                title: Text('Notification Settings'),
+                value: true,
+                onChanged: (value) {},
+              ),
+              SizedBox(height: 24),
+              Text(
+                'Account Settings',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Edit Profile'),
+                onTap: () {
+                  // Handle edit profile
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.lock),
+                title: Text('Change Password'),
+                onTap: () {
+                  // Handle change password
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text('Delete Account'),
+                onTap: () {
+                  // Handle delete account
+                },
+              ),
+              SizedBox(height: 24),
+              Text(
+                'About',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text('App Version'),
+                subtitle: Text('1.0.0'),
+              ),
+              ListTile(
+                leading: Icon(Icons.help),
+                title: Text('FAQs'),
+                onTap: () {
+                  // Handle FAQs
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.contact_support),
+                title: Text('Contact Us'),
+                onTap: () {
+                  // Handle contact us
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
