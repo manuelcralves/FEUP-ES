ef0c9a32cad8e1999ce8ed27805c2f5cecdbe118
manuelcralves <62514015+manuelcralves@users.noreply.github.com>
2023-05-21 20:34:29 +0100

    music page

--- app/lib/Music/MusicPage.dart
+++ app/lib/Music/MusicPage.dart
@@ -1,17 +1,77 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../Classes/Music.dart';
 
-class MusicPage extends StatefulWidget{
-
+class MusicPage extends StatelessWidget {
   final Music music;
 
   MusicPage({required this.music});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Music Page'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              music.getName(),
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Artist:',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              music.getArtist(),
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Album:',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              music.getAlbum().toString(),
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Lyrics:',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              music.getLyrics(),
+              style: TextStyle(
+                fontSize: 20,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
